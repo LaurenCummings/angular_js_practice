7f8f591fdb2daf1c0dc7d2d48ca23d619257b94b
LaurenCummings <LaurenCummings@users.noreply.github.com>
2016-04-28 00:47:16 -0400

    Added files via upload
    added filters (currency, uppercase, lowercase, orderBy)

---
+++ js/exam7.js
@@ -0,0 +1,33 @@
+var app7 = angular.module('app7', []);
+
+app7.controller('mainCtrl', function($scope) {
+
+	$scope.students = [
+		{name: "George Thomas", gpa: 3.5},
+		{name: "Susy Smith", gpa: 3.6},
+		{name: "Paul Marks", gpa: 3.2},
+		{name: "Sue Edgar", gpa: 3.8}
+	];
+
+	$scope.student = {
+		gpas:[
+			{name: "George Thomas", gpa: 3.5},
+			{name: "Susy Smith", gpa: 3.6},
+			{name: "Paul Marks", gpa: 3.2},
+			{name: "Sue Edgar", gpa: 3.8}
+		]
+	};
+
+	$scope.currDate = new Date();
+
+	$scope.randomStr = "Lorem ipsum dolor sit amet, consectetur";
+
+	$scope.randArray = [
+		"Tomato",
+		"Potato",
+		"Bread",
+		"Pickles",
+		"Raisins"
+	];
+
+});
