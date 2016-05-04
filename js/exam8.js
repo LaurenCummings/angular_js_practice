5056d90bbe4502fd5cc49ffa6aada8219b4050a5
LaurenCummings <LaurenCummings@users.noreply.github.com>
2016-05-03 23:26:09 -0400

    Added files via upload
    fixed a bug with hooking angular file
    added some javascript to change color of element

--- js/exam8.js
+++ js/exam8.js
@@ -1,3 +1,21 @@
 angular.module('app8', [])
 
-.directive("jqlDirective")
+.directive("jqlDirective", function() {
+	return function(scope, element, attrs) {
+		var players = element.children();
+
+		var listOfPlayers = "";
+
+		for(i = 0; i < players.length; i++){
+			if(players.eq(i).text() == "Barry Bonds"){
+				players.eq(i).css("color", "red");
+
+				players.eq(i).attr("number", "25");
+			}
+		}
+	}
+})
+
+.controller("mainCtrl", function($scope) {
+	
+})
