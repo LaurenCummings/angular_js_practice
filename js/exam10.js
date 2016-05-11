f1858494e0fc888559f62f3d2751e048d1b55902
LaurenCummings <LaurenCummings@users.noreply.github.com>
2016-05-10 23:07:44 -0400

    Add files via upload

--- js/exam10.js
+++ js/exam10.js
@@ -1,2 +1,17 @@
-angular.module('app10', [])
+var app10 = angular.module('app10', []);
+
+app10.directive("player", function() {
+	
+	var directive = {};
+
+	directive.restrict = 'E';
+
+});
+
+app10.controller("mainCtrl", function($scope) {
+	$scope.barryBonds = {name: "Barry Bonds", avg: 0.298, hr: 762, obp: 0.444};
+	$scope.hankAaron = {name: "Hank Aaron", avg: 0.305, hr: 755, obp: 0.374};
+	$scope.babeRuth = {name: "Babe Ruth", avg: 0.342, hr: 714, obp: 0.474};
+	$scope.tedWilliams = {name: "Ted Williams", avg: 0.344, hr: 521, obp: 0.482};
+});
 	
